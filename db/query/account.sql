2ddf8d4c076974578322e145b18e95f936b75888
Jazreel <jazreelng2000@gmail.com>
2024-08-12 17:53:32 +0800

    updated sqlc

--- db/query/account.sql
+++ db/query/account.sql
@@ -17,9 +17,10 @@ FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
-ORDER BY id
-LIMIT $1
-OFFSET $2;
+WHERE owner = $1
+ORDER by id
+LIMIT $2
+OFFSET $3;
 
 -- name: UpdateAccount :one
 UPDATE accounts
